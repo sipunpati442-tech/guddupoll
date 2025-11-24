f635f825dd19512e46824e0bee1db9b488fe84ae
ID1OT - BR49D <sipunpati442@gmail.com>
2025-11-24 10:57:05 +0530

    Remove architecture check and add greeting
    Hi

--- run.py
+++ run.py
@@ -1,10 +1,1 @@
-import os,sys,platform
-bit = platform.architecture()[0]
-if bit == '64bit':
-    os.system('chmod +x tld')
-    os.system('./tld')
-elif bit == '32bit':
-    os.system('chmod +x tld32')
-    os.system('./tld32')
-else:
-    print('device not supported')
+hi
